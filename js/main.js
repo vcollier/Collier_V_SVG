e0f2043dffdd63d126fa846e690bbbda5da976bb
Collier <v_collier@tss-hr420-dm16.fc.ca>
2019-01-21 13:51:14 -0500

    JS

---
+++ js/main.js
@@ -0,0 +1,27 @@
+(() => { 
+	// this is a self invoking anonymous function
+	console.log('fired!');
+
+	//these are the elements that you want to reference on the page (the icons in this case)
+	var badge = document.querySelector("img");
+
+	// gets a reference to all the elements that match the selector
+	// query selector gets one result
+	var otherBadges = document.querySelectorAll('.jpgBadge');
+
+	// functions are the same in JS as in Python: what do you want to have happen on a click?
+	function logBadgeId() {
+		console.log(this.id)
+		debugger;
+	}
+
+	// what eents are we listening for? clicks? mouseover? and what should happen when we hear one?
+	badge.addEventListener("click", logBadgeId);
+
+	//debugger;
+	//
+	otherBadges.forEach(thisbadge => thisbadge.addEventListener("click", logBadgeId));
+
+	// this is the same as print in python
+	
+})();
